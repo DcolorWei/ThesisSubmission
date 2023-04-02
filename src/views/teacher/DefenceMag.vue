df845479707c3e355c3d662b88255e7759509bb2
unknown <corfer@yeah.net>
2023-04-02 22:59:54 +0800

    feature: 答辩组增改完成

--- src/views/teacher/DefenceMag.vue
+++ src/views/teacher/DefenceMag.vue
@@ -9,11 +9,11 @@
     <el-card body-style="width:85vw" style="margin: 15px 0;" v-for="item, index in defenceGroups" :key="index">
         <div style="width:90%;margin: 10px auto;">
             <el-descriptions :title="'ID: ' + item.id">
-                <el-descriptions-item label="主席">{{ item.chairman.name }}</el-descriptions-item>
+                <el-descriptions-item label="主席">{{ item.chairman?.name }}</el-descriptions-item>
                 <el-descriptions-item label="容量">{{ item.nowCapacity || 0 }}/{{ item.capacity || 'infinitas'
                 }}</el-descriptions-item>
-                <el-descriptions-item label="Remarks">
-                    <el-tag size="small">Copied</el-tag>
+                <el-descriptions-item label="标记">
+                    <el-tag size="small">Available</el-tag>
                 </el-descriptions-item>
                 <el-descriptions-item label="时间">{{ item.defenceTime }}</el-descriptions-item>
                 <el-descriptions-item label="地点">{{ item.defencePlace }}</el-descriptions-item>
@@ -37,7 +37,7 @@
                 </el-table-column>
             </el-table>
             <div style="display:flex;justify-content: flex-end;margin-top: 20px;">
-                <el-button type="warning" plain>编辑</el-button>
+                <el-button type="warning" plain @click="() => edit(item)">编辑</el-button>
                 <el-button type="danger" plain>删除</el-button>
             </div>
         </div>
@@ -59,7 +59,7 @@
                 <el-form-item label="委员">
                     <el-select v-model="form.committee" filterable multiple placeholder="请选择" style="width: 80%;">
                         <el-option v-for="item in teacherInfo" :key="item.id" :label="item.name + ' ' + item.teacherId"
-                            :value="item.id + ' ' + item.id"></el-option>
+                            :value="item.id"></el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="时间">
@@ -69,6 +69,15 @@
                 <el-form-item label="地点">
                     <el-input v-model="form.defencePlace" style="width: 80%;"></el-input>
                 </el-form-item>
+
+                <!-- 容量，现数量 -->
+                <el-form-item label="容量">
+                    <el-input-number v-model="form.capacity" style="width: 80%;"></el-input-number>
+                </el-form-item>
+                <el-form-item label="现数量">
+                    <el-input-number v-model="form.nowCapacity" style="width: 80%;"></el-input-number>
+                </el-form-item>
+
             </el-form>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="() => dialogVisible = false">取消</el-button>
@@ -83,7 +92,7 @@ import axios from 'axios';
 import { Ref, ref, reactive } from 'vue'
 import { DefenceInfo } from '~/entity/base/Defence';
 import webApi from '~/util/webApi';
-import { GetDefenceGroupsRes, GetTeacherInfoRes } from '~/util/webRes';
+import { GetDefenceGroupsRes, GetTeacherInfoRes, UsualRes } from '~/util/webRes';
 
 const defenceGroups = reactive([] as DefenceInfo[])
 webApi.post<GetDefenceGroupsRes>('/getDefenceGroupInfo', {}).then(res => {
@@ -103,29 +112,75 @@ function getTeacherInfo(pageIndex = 1) {
 getTeacherInfo()
 const dialogVisible = ref(false)
 const form = reactive({
+    id: '',
     chairman: '',
     secretary: '',
-    committee: [],
+    committee: [] as any[],
     defenceTime: '',
-    defencePlace: ''
+    defencePlace: '',
+    //容量，现数量
+    capacity: 0,
+    nowCapacity: 0
 })
 
 const add = () => {
     //清空原表单
+    form.id = ''
     form.chairman = ''
     form.secretary = ''
     form.committee = []
     form.defenceTime = ''
     form.defencePlace = ''
+    form.capacity = 0
+    form.nowCapacity = 0
 
     dialogVisible.value = true;
 }
 
-const save = () => {
-    webApi.post<GetDefenceGroupsRes>('/getDefenceGroupInfo', {}).then(res => {
+const edit = (item: any) => {
+    //清空原表单
+    form.chairman = ''
+    form.secretary = ''
+    form.defenceTime = ''
+    form.defencePlace = ''
+    form.capacity = 0
+    form.nowCapacity = 0
+
+    while (form.committee.length) {
+        form.committee.pop()
+    }
+
+    //填充表单
+    form.id = item.id;
+    form.chairman = item.chairman.id
+    form.secretary = item.secretary.id
+    form.defenceTime = item.defenceTime
+    form.defencePlace = item.defencePlace
+    form.committee.push(...item.committee.map((item: any) => item.id))
+    form.capacity = item.capacity
+    form.nowCapacity = item.nowCapacity
+
+    dialogVisible.value = true;
+}
+
+const save = async () => {
+    //若form存在id字段则为编辑，否则为新建
+
+    //将时间转换为yyyy-MM-dd HH:mm:ss格式
+    form.defenceTime = new Date(form.defenceTime).toLocaleString().replaceAll('/', '-')
+
+    if (form.id) {
+        await webApi.post<UsualRes>('/updateDefenceGroupInfo', form)
+    } else {
+        await webApi.post<UsualRes>('/createDefenceGroupInfo', form)
+    }
+    await webApi.post<GetDefenceGroupsRes>('/getDefenceGroupInfo', {}).then(res => {
+        while (defenceGroups.length) {
+            defenceGroups.pop()
+        }
         defenceGroups.push(...res.data.data)
+        dialogVisible.value = false
     })
-    dialogVisible.value = false
 }
 
 
