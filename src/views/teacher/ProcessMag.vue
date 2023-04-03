e5d155c1448cc98d2aea3d7967a5664d87d50eca
unknown <corfer@yeah.net>
2023-04-03 13:44:17 +0800

    feature: get unique result which used to update data by search

--- src/views/teacher/ProcessMag.vue
+++ src/views/teacher/ProcessMag.vue
@@ -6,43 +6,39 @@
     <div style="margin: 30px 0;width: 87vw;">
         <el-row :gutter="20">
             <el-col :span="4">
-                <el-input placeholder="学号"></el-input>
+                <el-input v-model="studentId" placeholder="学号"></el-input>
             </el-col>
             <el-col :span="4">
-                <el-input placeholder="姓名"></el-input>
-            </el-col>
-            <el-col :span="4">
-                <el-input placeholder="指导教师"></el-input>
+                <el-input v-model="flowId" placeholder="流程号"></el-input>
             </el-col>
             <el-col :span="3">
-                <el-button>筛选</el-button>
+                <el-button @click="search()">搜索</el-button>
             </el-col>
         </el-row>
     </div>
     <h3 style="color:#606266;width: 90%;margin-top: 20px;text-align: left">学生信息</h3>
-    <el-card body-style="width:85vw">
-        <el-table
-            :data="[{ id: student.studentId, name: student.name, phone: student.phoneNumber, email: student.emailAddress }]"
-            style="width: 90vw">
+    <el-card v-if="flow.id" body-style="width:85vw">
+        <el-table :data="[{ id: flow.studentId, name: flow.studentName }]" style="width: 90vw">
             <el-table-column prop="id" label="学号" width="200" />
             <el-table-column prop="name" label="姓名" width="200" />
-            <el-table-column prop="phone" label="电话" width="200" />
-            <el-table-column prop="email" label="邮箱" />
-
-        </el-table>
-
-        <el-table style="width: 90vw">
-            <el-table-column>
+            <el-table-column label="流程状态">
                 <template #default>
-                    <el-tag type="warning" round>{{ status.status }}</el-tag>
+                    <el-tag type="warning" round>{{ flow.status }}</el-tag>
                 </template>
             </el-table-column>
         </el-table>
     </el-card>
+    <!-- 暂无学生与流程的信息 -->
+    <el-card body-style="width:85vw" v-else>
+        <div style="display: flex;justify-content: center;align-items: center;height: 50px;">
+            <h4 style="color:#606266">暂无匹配的学生与流程的信息</h4>
+        </div>
+    </el-card>
 
     <h3 style="color:#606266;width: 90%;margin-top: 20px;text-align: left">关联教师</h3>
     <el-card body-style="width:85vw">
-        <el-table :data="[status.verifier, status.innerAuditor, status.outerAuditor1,status.outerAuditor2]" style="width: 90vw">
+        <el-table v-if="flow.verifier" style="width: 90vw"
+            :data="[flow.verifier, flow.innerAuditor, flow.outerAuditor1, flow.outerAuditor2]">
             <el-table-column prop="name" label="教师姓名" width="150" />
             <el-table-column label="身份" width="150">
                 <template #default="{ $index }">
@@ -67,7 +63,8 @@
         <div style="display:flex;justify-content: flex-end;">
             <el-button type="warning" plain>更换答辩组</el-button>
         </div>
-        <el-table :data="[status.defenceGroup?.chairman, status.defenceGroup?.secretary, ...status.defenceGroup!.committee]"
+        <el-table v-if="flow.defenceGroup"
+            :data="[flow.defenceGroup?.chairman, flow.defenceGroup?.secretary, ...flow.defenceGroup!.committee]"
             style="width: 90vw">
             <el-table-column prop="name" label="教师姓名" width="150" />
             <el-table-column label="身份" width="150">
@@ -89,23 +86,47 @@
 </template>
 
 <script lang="ts" setup>
-import axios from 'axios';
 import { Ref, ref, reactive } from 'vue'
 import { ProcessDetail } from '~/entity/base/Process';
+import { FlowStatus } from '~/entity/enum/Flow';
 import webApi from '~/util/webApi';
-import { ProcessStatusRes, StudentInfoRes } from '~/util/webRes';
+import { GetFlowDetailRes } from '~/util/webRes';
 
-
-const student: any = ref({})
-webApi.post<StudentInfoRes>('/getStudentInfoBy', {}).then((data) => {
-    student.value = data[0]
-    console.log(student.value)
+const flow: ProcessDetail = reactive({
+    id: 0,
+    status: FlowStatus.FLOW_START,
+    verifier: null,
+    innerAuditor: null,
+    outerAuditor1: null,
+    outerAuditor2: null,
+    defenceGroup: null,
+    studentName: '',
+    studentId: null,
 })
-const status: any = reactive({})
-axios.post('/getFlowInfo').then(({ data }) => {
-    Object.keys(data[0]).forEach((k: any) => status[k] = data[0][k])
-})
 
+const studentId: Ref<string> = ref('')
+const flowId: Ref<string> = ref('')
+
+//触发search事件，搜索流程信息
+const search = () => {
+    webApi.post<GetFlowDetailRes>('/getFlowInfo', {
+        id: Number(flowId.value),
+    }).then(res => {
+        //只有流程结果数量为1时，才会赋值，否则报错
+        if (res.data.data.length != 1) {
+            alert('无唯一匹配结果！')
+            throw new Error('流程数量不为1')
+        } else {
+            const target = res.data.data[0]
+            //遍历每一个属性，将其赋值给flow
+            if (target)
+                Object.keys(target).forEach(key => {
+                    //@ts-ignore
+                    flow[key] = target[key]
+                })
+        }
+    })
+}
 
 </script>
 <style>
