27496e497043155a36f733a3e42291b52363dcf6
unknown <corfer@yeah.net>
2023-04-06 20:02:45 +0800

    config: 引入pinia&dingtalk

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import "~/styles/index.scss";
 import 'uno.css'
 import "element-plus/theme-chalk/src/message.scss"
 import { router } from "./route";
+import { createPinia } from 'pinia'
+
+const pinia = createPinia();
 
 const app = createApp(App);
-app.use(router).mount("#app");
+app.use(router).use(pinia).mount("#app");
+
 
