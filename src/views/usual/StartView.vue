dfbf9344bcf191f2379e4fa75cfaf3ada7ab68ba
dcolor <corfer@yeah.net>
2023-04-22 11:49:17 +0800

    feature: 登录检查

--- src/views/usual/StartView.vue
+++ src/views/usual/StartView.vue
@@ -11,15 +11,21 @@ import { Role } from "~/entity/enum/Role";
 import { ElMessage } from "element-plus";
 
 setTimeout(() => {
-    if (useAuthStore().token !== '' || useAuthStore().roles.length !== 0) {
-        const role = useAuthStore().roles;
-        if (role.includes(Role.STUDENT)) {
-            ElMessage.success("欢迎登录，您的身份是：学生");
-            router.push("/thesisstatus");
-        } else {
-            ElMessage.success("欢迎登录，您的身份是：教师");
-            router.push("/processmag");
+    try {
+        if (useAuthStore().token !== '' && useAuthStore().roles.length !== 0) {
+            const role = useAuthStore().roles;
+            if(role&&role[0]==Role.STUDENT){
+                router.push("/thesisstatus");
+            }else if (role && role.length) {
+                ElMessage.success("欢迎登录，您的身份是：" + role[0]);
+                router.push("/processmag");
+            } else {
+                ElMessage.error("您的身份状态缺失，请联系管理员");
+            }
         }
+    } catch {
+        ElMessage.error("您的身份状态有误，请联系管理员");
     }
+
 }, 1000)
 </script>
