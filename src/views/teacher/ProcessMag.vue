7babb92ed8c09c0ea43fb7faab8ad1f30578da51
dcolor <corfer@yeah.net>
2023-04-26 15:06:06 +0800

    fix: 确认老师字段

--- src/views/teacher/ProcessMag.vue
+++ src/views/teacher/ProcessMag.vue
@@ -106,8 +106,8 @@
                             {{ flowsFilter[flowIndex].verifierPass ? 'pass' : 'fail' }}
                         </el-tag> -->
                         <el-tag v-if="$index == 0 && flowsFilter[flowIndex]?.verifier"
-                            :type="flowsFilter[flowIndex].verifierPass == true ? 'success' : flowsFilter[flowIndex].verifyPass == false ? 'danger' : 'warning'">
-                            {{ flowsFilter[flowIndex].verifierPass == true ? 'pass' : flowsFilter[flowIndex].verifyPass ==
+                            :type="flowsFilter[flowIndex].verifyPass == true ? 'success' : flowsFilter[flowIndex].verifyPass == false ? 'danger' : 'warning'">
+                            {{ flowsFilter[flowIndex].verifyPass == true ? 'pass' : flowsFilter[flowIndex].verifyPass ==
                                 false ? 'fail' : 'waiting' }}
                         </el-tag>
                         <el-tag v-else-if="$index == 1 && flowsFilter[flowIndex]?.innerAuditor"
